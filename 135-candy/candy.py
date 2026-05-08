dd0c852d8d4acb454b7b9efcfa1d2b6999dcb2e1
Yuktha K N <yukthanagaraj13@gmail.com>
2026-05-08 17:54:21 +0530

    Time: 21 ms (58.32%) | Memory: 14.8 MB (21.33%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def candy(self, ratings):
+        n = len(ratings)
+
+        # Initially give 1 candy to everyone
+        candies = [1] * n
+
+        # Left to right pass
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Right to left pass
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)
