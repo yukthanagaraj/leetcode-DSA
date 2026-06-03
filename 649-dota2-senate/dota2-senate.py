944603a20315e2dc03f9bc571ea1a88cacb7172e
Yuktha K N <yukthanagaraj13@gmail.com>
2026-06-03 20:14:37 +0530

    Time: 19 ms (84.01%) | Memory: 12.6 MB (91.85%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate):
+        n = len(senate)
+
+        radiant = deque()
+        dire = deque()
+
+        for i, ch in enumerate(senate):
+            if ch == 'R':
+                radiant.append(i)
+            else:
+                dire.append(i)
+
+        while radiant and dire:
+            r = radiant.popleft()
+            d = dire.popleft()
+
+            if r < d:
+                radiant.append(r + n)
+            else:
+                dire.append(d + n)
+
+        return "Radiant" if radiant else "Dire"
