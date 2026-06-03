a1c5034482ca1b8bb1de99e1ecf77cba97e7edc4
Yuktha K N <yukthanagaraj13@gmail.com>
2026-06-03 20:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (48.22%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def judgeCircle(self, moves):
+        return (
+            moves.count('U') == moves.count('D')
+            and moves.count('L') == moves.count('R')
+        )
