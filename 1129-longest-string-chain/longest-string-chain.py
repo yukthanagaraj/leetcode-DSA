1687c2e137457d3e8633ec594d882fb600db6bbd
Yuktha K N <yukthanagaraj13@gmail.com>
2026-07-13 21:29:59 +0530

    Time: 51 ms (66.67%) | Memory: 12.7 MB (16.34%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestStrChain(self, words):
+        words.sort(key=len)
+
+        dp = {}
+        ans = 1
+
+        for word in words:
+            dp[word] = 1
+
+            for i in range(len(word)):
+                prev = word[:i] + word[i+1:]
+
+                if prev in dp:
+                    dp[word] = max(dp[word], dp[prev] + 1)
+
+            ans = max(ans, dp[word])
+
+        return ans
