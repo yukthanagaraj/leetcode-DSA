a559cbafc9137fcd417dc4fbba5d160a18eecf88
Yuktha K N <yukthanagaraj13@gmail.com>
2026-07-23 21:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (97.05%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkIfExist(self, arr):
+        seen = set()
+
+        for x in arr:
+            if 2 * x in seen or (x % 2 == 0 and x // 2 in seen):
+                return True
+            seen.add(x)
+
+        return False
