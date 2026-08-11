b6c6738159dfc6ac810f6a9fc1d7175310183b8f
Yuktha K N <yukthanagaraj13@gmail.com>
2026-08-11 21:16:31 +0530

    Time: 69 ms (20.30%) | Memory: 16 MB (2.42%) - LeetSync

---
+++ 384-shuffle-an-array/shuffle-an-array.py
@@ -0,0 +1,19 @@
+import random
+
+class Solution:
+
+    def __init__(self, nums):
+        self.original = nums[:]
+
+    def reset(self):
+        return self.original[:]
+
+    def shuffle(self):
+        arr = self.original[:]
+
+        for i in range(len(arr) - 1, 0, -1):
+            j = random.randint(0, i)
+
+            arr[i], arr[j] = arr[j], arr[i]
+
+        return arr
